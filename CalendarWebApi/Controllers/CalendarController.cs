1e83806ae4b238ee8a78c4e2142c83926373c68f
Anand Shah <anand_shah06@outlook.com>
2020-11-30 04:01:41 +0530

    Test 1

--- CalendarWebApi/Controllers/CalendarController.cs
+++ CalendarWebApi/Controllers/CalendarController.cs
@@ -7,6 +7,49 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CalendarWebApi.DTO;
 
-namespace CalendarWebApi.Controllers
+namespace CalendarWebApi.Controllers 
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+        private readonly IRepository repository; 
+        public CalendarController(IRepository repositoryParam)
+        {
+            repository = repositoryParam;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Calendar calendarParam)
+        {
+            var calendar =  await repository.AddEvent(calendarParam);
+            return StatusCode(201, calendar);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
 }
