ca6090f27a0df0da84d613903f3347f12d905f27
Anand Shah <anand_shah06@outlook.com>
2020-12-04 22:00:56 +0530

    Conpleted.

--- CalendarWebApi/Controllers/CalendarController.cs
+++ CalendarWebApi/Controllers/CalendarController.cs
@@ -19,15 +19,15 @@ namespace CalendarWebApi.Controllers
             repository = repositoryParam;
         }
         // GET api/values
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Calendar>>> Get()
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get()
         {
-            List<Calendar> calendars = await repository.GetCalendar();
+            var calendars = await repository.GetCalendar();
             return StatusCode(200, calendars);
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
+        [HttpGet]
         public async Task<ActionResult> Get(int id)
         {           
             EventQueryModel eventQueryModel = new EventQueryModel();
@@ -36,7 +36,7 @@ namespace CalendarWebApi.Controllers
             return StatusCode(200, calendars.ToArray());
         }
                 
-        [HttpGet()]
+        [HttpGet]
         [Route("query")]
         public async Task<ActionResult> Get(string location)
         {         
@@ -46,14 +46,31 @@ namespace CalendarWebApi.Controllers
             var calendar = await repository.GetCalendar(eventQueryModel);
             return StatusCode(200, calendar.ToArray());
           
-        }        
-        
+        }
+
+        [HttpGet]
+        [Route("sort")]
+        public async Task<ActionResult> Sort()
+        {
+            var calendars = await repository.GetEventsSorted();
+            return StatusCode(200, calendars);
+        }
 
         // PUT api/values/5
-        [HttpPut()]
-        public async Task<ActionResult> Put(EventQueryModel eventQueryModel)
+        [HttpPost]
+        public async Task<ActionResult> Post(Calendar calendar)
         {
-            var calendar = new Calendar {Name = "ChangedEvent3", Location  = "Alaska" };
+            
+            var calendarReturn = await repository.AddEvent(calendar);
+            return StatusCode(201, calendarReturn);
+        }
+
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(Calendar calendar)
+        {
+            //var calendar = new Calendar {Name = "ChangedEvent3", Location  = "Alaska" };
             var calendarReturn = await repository.UpdateEvent(calendar);
             return StatusCode(204, calendarReturn);
         }
@@ -71,7 +88,7 @@ namespace CalendarWebApi.Controllers
             if (doCalendarExist)
             {
                 var calendar = await repository.DeleteEvent(calendars[calendars.FindIndex(c => c.Id == id)]);
-                return StatusCode(200);
+                return StatusCode(204);
             }
             else
             {
